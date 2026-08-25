9158b3910b849f28d02f95669bd8bd83325d4faa
betheaakashhh <karmanam108@gmail.com>
2026-08-25 14:23:42 +0530

    test

--- src/merRoute.js
+++ src/merRoute.js
@@ -3,5 +3,8 @@ const router = require('express').Router();
 router.get('/api/mergese', (req, res) => {
   res.json({ ok: true });
 });
+router.get("/api/testing/merger", (req, res) =>{
+    res.json({ ok: true });
+})
 
 module.exports = router;
