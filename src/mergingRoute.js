dedf2c987fa5925ad729f3fee284ccf281ba4755
betheaakashhh <karmanam108@gmail.com>
2026-08-27 02:33:38 +0530

    erge

--- src/mergingRoute.js
+++ src/mergingRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.get('/api/merging', (req, res) => {
   res.json({ ok: true });
+  console.log("GET:/api/merging");
 });
 
 module.exports = router;
